8a476d1b07bcc7d2844354418606dbed83f825f6
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2020-03-08 21:36:08 +0530

    Update model.py

--- model.py
+++ model.py
@@ -18,6 +18,7 @@ except Exception:
 import json
 import tensorflow as tf
 
+
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
