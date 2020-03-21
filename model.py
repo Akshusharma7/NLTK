c6d3f0b5cef9554bb36583441eb5f3809022e023
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2020-03-21 10:42:09 +0530

    Update model.py

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ Original file is located at
     https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/TensorFlow%20In%20Practice/Course%203%20-%20NLP/Course%203%20-%20Week%202%20-%20Lesson%202.ipynb
 """
 
+
 # Commented out IPython magic to ensure Python compatibility.
 # Run this to ensure TensorFlow 2.x is used
 try:
